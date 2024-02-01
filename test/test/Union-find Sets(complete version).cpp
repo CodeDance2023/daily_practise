a0c13b9fb17035da771d712b2196e3213463a861
AiPrograming <lqw2860689098@qq.com>
2024-02-01 19:21:25 +0800

    并查集算法完整版

---
+++ test/test/Union-find Sets(complete version).cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int fa[1000000] = { 0 };   //fa数组储存每一个元素的上级是谁
+int level[1000000] = { 1 };   //leve数组储存每一个元素在集合中的等级是多少，最低为1
+void init(int N)              //init函数用来初始化每一个元素的上级和它们在集合中的等级
+{
+	int i = 0;
+	for (i = 1; i <= N; i++)
+	{
+		fa[i] = i;               //每一个元素的上级一开始都统一初始化为自己
+		level[i] = 1;            //每一个元素的等级一开始都统一初始化为1
+	}
+}
+
+int find(int x)                   //join函数用来找到一个元素的boss（根节点）
+{
+	if (fa[x] == x)              //一个元素的上级为自己，说明他自己就是boss
+	{
+		return x;                //递归终止条件
+	}
+	else
+	{
+		fa[x] = find(fa[x]);              //路径压缩算法!!! 在查找一个元素的boss时，可以将这个元素到boss之间的所有元素的boss都直接设置为boss，减少“树”的高度
+		return fa[x];                     //并且减少下次查找这条路径上元素时候调用find函数的次数
+	}
+}
+
+
+void join(int x, int y)      //join函数用来合并两个元素（指定一个元素为另一个元素的上级）
+{
+	int fx, fy;        //fx,fy分别为x和y的boss
+	fx = find(x);
+	fy = find(y);
+	if (fx != fy)           //boss不同说明不在同一个集合，要合并
+	{
+		if (level[fx] > level[fy])
+		{
+			fa[fy] = fx;
+		}
+		else              //因为等级相同时可以随便指定谁是谁的上级，因此等级相同和大于两种情况放在一个else内
+		{
+			if (level[fx] == level[fy])
+			{
+				level[fy]++;
+			}
+			fa[fx] = fy;
+		}
+	}
+}
--- test/test/alg_union-find sets.c
+++ test/test/alg_union-find sets.c
@@ -1,61 +1,64 @@
 #define _CRT_SECURE_NO_WARNINGS
-//并查集算法
-#include <stdio.h>
-int fa[1000] = { 0 };
-void init(int n)
-{
-	int i = 0;
-	for (i = 1; i <= n; i++)
-	{
-		fa[i] = i;
-	}
-}
-int find(int x)
-{
-	if (fa[x] == x)
-	{
-		return x;
-	}
-	else
-	{
-		fa[x] = find(fa[x]);    //路径压缩
-		return fa[x];
-	}
-}
-void join(int x, int y)
-{
-	int fx, fy;
-	fx = find(x);
-	fy = find(y);
-	if (fx != fy)
-	{
-		fa[fx] = fy;
-	}
-}
+//并查集算法(路径压缩版)
+//#include <stdio.h>
+//int fa[1000] = { 0 };       //fa表示上级
+//void init(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		fa[i] = i;                  //初始化是每个人的上级都是自己
+//	}
+//}
+//int find(int x)              //查找教主，而不是查找一个节点的上级
+//{
+//	if (fa[x] == x)
+//	{
+//		return x;
+//	}
+//	else
+//	{
+//		fa[x] = find(fa[x]);    //路径压缩：通过递归，在查找一个节点的教主的过程中，就可以把所有人的上级都直接设置为教主
+//		return fa[x];
+//	}
+//}
+//void join(int x, int y)
+//{
+//	int fx, fy;
+//	fx = find(x);
+//	fy = find(y);
+//	if (fx != fy)
+//	{
+//		fa[fx] = fy;
+//	}
+//}
+//
+//int main()
+//{
+//	int n, m, c, d, q;
+//	int x, y;
+//	scanf("%d", &n);
+//	init(n);           //初始化
+//	scanf("%d", &m);
+//	for (int i = 1; i <= m; i++)     //给出前提条件关系
+//	{
+//		scanf("%d %d", &x, &y);
+//		join(x, y);
+//	}
+//	scanf("%d", &q);     //问题
+//	for (int j = 1; j <= q; j++)
+//	{
+//		scanf("%d %d", &c, &d);
+//		if (find(c) == find(d))   //找教主，教主相同则证明有关系,而不是找上级是否相同（fa[c] == fa[d]）
+//		{
+//			printf("yes\n");
+//		}
+//		else
+//		{
+//			printf("no\n");
+//		}
+//	}
+//}
 
-int main()
-{
-	int n, m, c, d, q;
-	int x, y;
-	scanf("%d", &n);
-	init(n);           //初始化
-	scanf("%d", &m);
-	for (int i = 1; i <= m; i++)     //给出前提条件关系
-	{
-		scanf("%d %d", &x, &y);
-		join(x, y);
-	}
-	scanf("%d", &q);     //问题
-	for (int j = 1; j <= q; j++)
-	{
-		scanf("%d %d", &c, &d);
-		if (find(c) == find(d))   //找教主，教主相同则证明有关系
-		{
-			printf("yes\n");
-		}
-		else
-		{
-			printf("no\n");
-		}
-	}
-}
+
+
